dc2fbd7c5bff60888385c880597b45139b5bf4a8
darkshark007 <darkshark_007@yahoo.com>
2025-04-07 20:38:18 -0600

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -23,8 +23,20 @@ function parseDataPackage(data) {
   return parsedData;
 }
 
-function exportDataPackage(data) {
+function buildDataPackage() {
+  let nameElement = document.getElementById('name');
+  let parsedData = makeDataPackage();
+  parsedData['name'] = nameElement.value;
+  // parsedData['author'] = data['author'];
+  // parsedData['url'] = data['url'];
+  // parsedData['ingredients'] = data['ingredients'];
+  // parsedData['steps'] = data['steps'];
+  return parsedData;
+}
+
+function exportDataPackage() {
   let params = new URL(window.location.href);
+  let data = buildDataPackage();
   let encoded = atob(data);
   params.searchParams.append('d', encoded);
 }
@@ -32,4 +44,18 @@ function exportDataPackage(data) {
 let params = new URLSearchParams(document.location.search);
 let data = parseDataPackage(params.get("d"));
 
+function buildForm() {
+  let form = document.createElement('FORM');
+  let nameElement = document.createElement('INPUT');
+  form.appendChild(nameElement);
+  nameElement.type='TEXT';
+  nameElement.name='name';
+  nameElement.id='name';
+  nameElement.onchange=exportDataPackage;
 
+  
+  // Set up based on the current form version
+  nameElement.value=data['name'];
+
+  window.document.appendChild(form);
+}
